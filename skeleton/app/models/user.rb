22a78ba4d815aa7b942f1902d660e594322271e5
pierre <pnavarin@gmail.com>
2021-09-20 14:05:28 -0400

    Created User Model and Table'

---
+++ skeleton/app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+    after_initialize :ensure_session_token
+
+    validates :session_token, presence: true, uniqueness: true
+    validates :user_name, :password_digest, presence: true
+
+    def self.find_by_credentials(user_name, password)
+        user = User.find_by(user_name: user_name)
+
+        if user && user.is_password?(password)
+            user
+        else    
+            nil
+        end
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+end
